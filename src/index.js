c45e46d94f7538e21f1113f905461d1bc4ecb527
VladMeleshko <vladik4.98@mail.ru>
2019-03-22 01:34:09 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arr = str.split('');
+  let row = 0;
+  while (row < arr.length) {
+    for (let r = 0; r < bracketsConfig.length; r++) {
+        let open = bracketsConfig[r][0];
+        let close = bracketsConfig[r][1];
+        if (arr[row] === open) {
+          if(arr[row + 1] === close){
+            arr.splice(row, 2);
+            row -= 2;
+          }
+        } 
+      }
+    row++;
+  }
+  if (arr.length === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
